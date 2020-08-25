5bf4ea83322df445de45517a56714125d8e7c185
Helena <inger.helena@gmail.com>
2020-08-25 18:47:53 +0100

    created library.createGenreCount method, canCheckBookCountByGenre pass

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
     private ArrayList<Book> stock;
     private int capacity;
+    private HashMap<String, Integer> genreCount;
 
     public Library(int capacity) {
         this.stock = new ArrayList<Book>();
         this.capacity = capacity;
+        this.genreCount = new HashMap<String, Integer>();
     }
 
     public int stockCount() {
@@ -25,5 +28,19 @@ public class Library {
 
     public Book removeBook() {
         return this.stock.remove(0);
+    }
+
+    public void createGenreCount() {
+        for (Book book : this.stock) {
+            if (this.genreCount.containsKey(book.getGenre())) {
+                this.genreCount.put(book.getGenre(), this.genreCount.get(book.getGenre()) + 1);
+            } else {
+                this.genreCount.put(book.getGenre(), 1);
+            }
+        }
+    }
+
+    public int bookCountByGenre(Book book) {
+        return this.genreCount.get(book.getGenre());
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,11 +6,15 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
     private Library library;
     private Book book;
+    private Book book2;
+    private Book book3;
 
     @Before
     public void before() {
         library = new Library(100);
         book = new Book("The Book Thief", "Markus Zusak", "historical fantasy");
+        book2 = new Book("Jonathan Strange and Mr Norrell", "Susanna Clarke", "historical fantasy");
+        book3 = new Book("Misery", "Stephen King", "thriller");
     }
 
     @Test
@@ -28,8 +32,8 @@ public class LibraryTest {
     public void cannotAddBook() {
         library = new Library(2);
         library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
         assertEquals(2, library.stockCount());
     }
 
@@ -38,5 +42,15 @@ public class LibraryTest {
         library.addBook(book);
         library.removeBook();
         assertEquals(0, library.stockCount());
+    }
+
+    @Test
+    public void canCheckBookCountByGenre() {
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.createGenreCount();
+        assertEquals(2, library.bookCountByGenre(book));
+        assertEquals(1, library.bookCountByGenre(book3));
     }
 }

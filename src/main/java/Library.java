24ac126af08cfd1597de913beaf77974d3b85a02
Helena <inger.helena@gmail.com>
2020-08-25 16:52:03 +0100

    canCountStock pass

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> stock;
+
+    public Library() {
+        this.stock = new ArrayList<Book>();
+    }
+
+    public int countStock() {
+        return this.stock.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book = new Book("The Book Thief", "Markus Zusak", "historical fantasy");
+    }
+
+    @Test
+    public void canCountStock() {
+        assertEquals(0, library.countStock());
+    }
 }
